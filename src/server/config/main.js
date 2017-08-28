9e682c1b3ed0f5020da85e86e596e9dd6ee5c0b0
Joshua Nance <joshua.h.nance@gmail.com>
2017-08-28 15:13:31 -0500

    Add all dependencies for auth

--- src/server/config/main.js
+++ src/server/config/main.js
@@ -1,4 +1,3 @@
 module.exports = {
-  port: process.env.PORT || 3000,
-  secret: 'wolpertinger',
+  secret: 'wolpertinger'
 };
--- src/shared/config.js
+++ src/shared/config.js
@@ -1,4 +1,4 @@
-export const WEB_PORT = process.env.PORT || 8000;
+export const WEB_PORT = process.env.PORT || 3000;
 export const STATIC_PATH = '/static';
 export const APP_NAME = 'mindmap';
 
