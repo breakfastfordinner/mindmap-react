1edffe7ce248efcb1e1cb03bf3991f412a63509d
Lina Yang <t886515@hotmail.com>
2017-08-29 16:32:55 -0500

    A quick commit before rebase

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -17,40 +17,67 @@ import {
   Switch
 } from 'react-router-dom';
 
-const App = () =>
-    <div>
+// const App = () =>
+//     <div>
+//         <h1>Home</h1>
+//           <Switch>
+//             <Route exact path="/" render={()=><Home auth="213"/>} />
+//             <Route path="/canvas/:id" component={Canvas}/>
+//             <Route path="/auth" component={Auth} />
+//           </Switch>
+
+//         <footer>
+//             <Link to="/">Home </Link>
+//             <Link to="/auth">Auth</Link>
+//         </footer>
+
+//     </div>;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  componentWillMount() {
+    //an ajax call to check if session exist
+    
+
+  }
+
+  handleLogin(userObj) {
+    this.setState({
+      signedIn: true,
+      user: userObj
+    })
+    //send a post request to server and 
+  }
+
+
+
+  render() {
+    return (
+      <div>
         <h1>Home</h1>
           <Switch>
-            <Route exact path="/" render={() => <Home />} />
+            <Route exact path="/" render={()=><Home auth="213"/>} />
             <Route path="/canvas/:id" component={Canvas}/>
-            <Route path="/auth" render={() => <Auth />} />
+            <Route path="/auth" component={Auth} />
           </Switch>
-
 
         <footer>
             <Link to="/">Home </Link>
             <Link to="/auth">Auth</Link>
         </footer>
 
-    </div>;
-
-// class App extends React.Component {
-
-//   render() {
-//     return (
-//       <div>
-//         <Router>
-//           <Switch>
-//             <Route path="/auth" component={Auth} />
-//             <Route path="/" component={Home} />
-//           </Switch>
-//         </Router>
-//       </div>
+      </div>
 
 
-//     )
-//   }
-// }
+    )
+  }
+}
 
 
 export default App;
