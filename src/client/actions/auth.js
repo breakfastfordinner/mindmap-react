2a84530c7267a1df31a6fbfc51522a4b18f792cb
Joshua Nance <joshua.h.nance@gmail.com>
2017-08-30 11:30:40 -0500

    Add front-end actions for authentication and logging out

---
+++ src/client/actions/auth.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+const REQUESTS = {
+  register: 'http://localhost:8000/api/auth/register',
+  login: 'http://localhost:8000/api/auth/login',
+};
+const ERROR_CODES = {
+  register: 422,
+  login: 401,
+};
+
+const authenticateUser = async (username, password, { type }) => {
+  try {
+    const request = REQUESTS[type];
+    const user = { username, password };
+    const response = await axios.post(request, user);
+
+    cookies.set('user', response.data, { path: '/' });
+
+    return { status: 201 };
+  } catch (error) {
+    return { status: ERROR_CODES[type], error };
+  }
+};
+
+const logOutUser = () => {
+  cookies.remove('user');
+  return { status: 204 };
+};
+
+module.exports = {
+  authenticateUser: authenticateUser,
+  logOutUser: logOutUser,
+};
