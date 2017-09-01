999605643035a43e0b17f87f166398f44300cd60
Joshua Nance <joshua.h.nance@gmail.com>
2017-08-31 19:11:25 -0500

    Move router definition

--- src/server/index.js
+++ src/server/index.js
@@ -27,11 +27,10 @@ app.get('/', (req, res) => {
   res.send(renderApp(APP_NAME));
 });
 
+router(app);
 
 app.listen(WEB_PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' :
     '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`);
 });
-
-router(app);
