b2cdbd1ab91a820bf5ee27d46bf7e29602b65106
Ash Woodall <ash.woodall@gmail.com>
2017-09-12 10:55:50 -0500

    edit snackbar message to not allow root node removal

--- src/client/ToolDrawer.jsx
+++ src/client/ToolDrawer.jsx
@@ -3,12 +3,25 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right';
 import Divider from 'material-ui/Divider';
+import Slider from 'material-ui/Slider';
+
+
+const style = {
+  height: '46px',
+  width: '400px',
+  margin: '0 auto'
+};
 
 class ToolDrawer extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state={
+      firstSlider: 0.5,
+    }
   }
+
+
 
   componentDidMount() {
      //console.log('tool drawer props', this.props)
@@ -19,6 +32,10 @@ class ToolDrawer extends React.Component {
   toggleChecked() {
     this.setState({open: !this.props.open});
   }
+
+  handleFirstSlider(event, value){
+    this.setState({firstSlider: value});
+  };
 
   render() {
     return (
@@ -53,6 +70,14 @@ class ToolDrawer extends React.Component {
               <MenuItem primaryText="Curved" onClick={this.props.selectDiagonal} insetChildren={true}/>,
               <MenuItem primaryText="Elbow" onClick={this.props.selectElbow} insetChildren={true}/>,
               <MenuItem primaryText="Straight" onClick={this.props.selectStraight} insetChildren={true}/>,
+            ]}
+          />
+          <MenuItem
+
+            primaryText="Node Spacing"
+            rightIcon={<ArrowDropRight />}
+            menuItems={[
+                <Slider style={style} value={this.state.firstSlider} onChange={this.handleFirstSlider} />
             ]}
           />
 
--- src/client/tree/Tree/index.js
+++ src/client/tree/Tree/index.js
@@ -8,6 +8,8 @@ import clone from 'clone';
 import deepEqual from 'deep-equal';
 import uuid from 'uuid';
 import ReactTooltip from 'react-tooltip';
+import Snackbar from 'material-ui/Snackbar';
+
 
 
 import Node from '../Node';
@@ -29,6 +31,8 @@ export default class Tree extends React.Component {
     this.state = {
       initialRender: true,
       data: this.assignInternalProperties(clone(props.data)),
+      message: 'Node deleted',
+      open: false,
       depth: 0
 
     };
@@ -41,6 +45,7 @@ export default class Tree extends React.Component {
     this.createLocalSendData = this.createLocalSendData.bind(this);
     this.handleOnClick = this.handleOnClick.bind(this);
     this.handleRightClick = this.handleRightClick.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
     this.editMapHelper = this.editMapHelper.bind(this);
   }
 
@@ -260,6 +265,12 @@ export default class Tree extends React.Component {
       })
   }
 
+  handleRequestClose() {
+    this.setState({
+      open: false,
+    });
+  };
+
 
   createLocalSendData(data) {
 
@@ -299,7 +310,10 @@ export default class Tree extends React.Component {
 
   removeNode(nodeId, data) {
     if (data.id === nodeId) {
-      console.log('can not remove original node')
+      this.setState({
+        open: true,
+        message: 'Cannot remove root node'
+      });
       return [data];
     }
     if (data.children) {
@@ -308,7 +322,12 @@ export default class Tree extends React.Component {
           data.children.splice(i, 1);
           return [data]
         }
+        //console.log(data.children[i].name)
         this.removeNode(nodeId, data.children[i])
+        this.setState({
+          open: true,
+          message: 'Node removed'
+        });
       }
     }
     return [data];
@@ -440,6 +459,16 @@ export default class Tree extends React.Component {
           })}
           </g>
         </svg>
+
+        <Snackbar
+          open={this.state.open}
+          message={this.state.message}
+          autoHideDuration={1500}
+          onActionTouchTap={this.handleActionTouchTap}
+          onRequestClose={this.handleRequestClose}
+          action={'okay'}
+        />
+
       </div>
     );
   }
